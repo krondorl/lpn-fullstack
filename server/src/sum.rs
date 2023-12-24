ff5431f5e8a96cce29a9ccf15eb3cd83180ba11d
Adam Burucs <adam.burucs@gmail.com>
2023-12-24 14:49:35 +0100

    feat: add sum functions

---
+++ server/src/sum.rs
@@ -0,0 +1,19 @@
+pub fn sum_recursive(raw_number: u32) -> u32 {
+    if raw_number < 10 {
+        return raw_number;
+    }
+    sum_recursive(sum_numbers(raw_number))
+}
+
+pub fn sum_numbers(n: u32) -> u32 {
+    let mut sum: u32 = 0;
+    let binding = n.to_string();
+    let numbers_split = binding.split("");
+    for string_number in numbers_split {
+        match string_number.parse::<u32>() {
+            Ok(val) => sum += val,
+            Err(e) => println!("Error when casting to integer: {e}"),
+        }
+    }
+    sum
+}
