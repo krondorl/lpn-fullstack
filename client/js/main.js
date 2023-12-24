4ea9e39e011b8c29c0fe0591d48357a817de9f34
Adam Burucs <adam.burucs@gmail.com>
2023-12-24 16:53:10 +0100

    feat: prepare client files, delete unnecessary details

--- client/js/main.js
+++ client/js/main.js
@@ -7,21 +7,6 @@
  * Please see LICENSE file in the repo root folder.
  */
 
-import { sumRecursive } from "js/sum";
-
-function calculateLPNumber(birthTime) {
-  if (birthTime && birthTime.length === 10) {
-    const splitBirthTime = birthTime.split("");
-    const birthNumbers = splitBirthTime.filter(
-      (character) => character !== "-"
-    );
-    const joinedNumber = parseInt(birthNumbers.join(""));
-    return sumRecursive(joinedNumber);
-  } else {
-    console.error("Wrong birthTime ", birthTime);
-  }
-}
-
 const resultContainer = document.querySelector(".result");
 const resultSpan = document.querySelector(".result-number");
 const summaries = document.querySelectorAll("details");
@@ -48,13 +33,7 @@ document.querySelector("#submit").addEventListener("click", () => {
   summaries.forEach((summary) => (summary.open = false));
   const _date = document.querySelector("#date").value;
   if (_date && _date.length === 10) {
-    const lpNumber = calculateLPNumber(_date);
-    if (lpNumber > 0 && lpNumber < 10) {
-      resultContainer.style.display = "block";
-      resultSpan.textContent = lpNumber;
-      summaries[lpNumber - 1].classList.toggle("selected-mission-details");
-      summaries[lpNumber - 1].open = true;
-    }
+    // @todo GET Request
   } else {
     console.log("Wrong date ", _date);
   }
